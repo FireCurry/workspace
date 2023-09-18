bf3cd1e303788c683da50950b54e9f57b1e4c0f9
FireCurry <firecurry@naver.com>
2023-09-18 21:36:23 +0900

    20230918 집

--- 04_WebFront/3_JavaScript/js/test.js
+++ 04_WebFront/3_JavaScript/js/test.js
@@ -1,56 +1,78 @@
-const q =document.getElementById('q');
-const w =document.getElementById('w');
-const e =document.getElementById('e');
-const r =document.getElementById('r');
-console.log(document.key);
+// const q =document.getElementById('q');
+// const w =document.getElementById('w');
+// const e =document.getElementById('e');
+// const r =document.getElementById('r');
+// console.log(document.key);
 
 
-document.addEventListener('keydown', function(event){
-  new Function("if(event.key == '"+event.key+"'){ return "+event.key+".style.backgroundColor = 'black' "+event.key+".style.color = 'white'}")();
-  console.log(event.key);
-  // if(event.key == 'q'){
-  //   q.style.backgroundColor = "black"
-  //   q.style.color = "white"
-  // }
-  // if(event.key == 'w'){
-  //   w.style.backgroundColor = "black";
-  //   w.style.color = "white";
-  // }
-  // if(event.key == 'e'){
-  //   e.style.backgroundColor = "black";
-  //   e.style.color = "white";
-  // }
-  // if(event.key == 'r'){
-  //   r.style.backgroundColor = "black";
-  //   r.style.color = "white";
-  // }
+// document.addEventListener('keydown', function(event){
+//   new Function("if(event.key == '"+event.key+"'){ return "+event.key+".style.backgroundColor = 'black' "+event.key+".style.color = 'white'}")();
+//   console.log(event.key);
+//   // if(event.key == 'q'){
+//   //   q.style.backgroundColor = "black"
+//   //   q.style.color = "white"
+//   // }
+//   // if(event.key == 'w'){
+//   //   w.style.backgroundColor = "black";
+//   //   w.style.color = "white";
+//   // }
+//   // if(event.key == 'e'){
+//   //   e.style.backgroundColor = "black";
+//   //   e.style.color = "white";
+//   // }
+//   // if(event.key == 'r'){
+//   //   r.style.backgroundColor = "black";
+//   //   r.style.color = "white";
+//   // }
+// })
+// document.addEventListener('keyup', function(event){
+//   console.log(event.key);
+//   if(event.key == 'q'){
+//     q.style.backgroundColor = "white"
+//     q.style.color = "black"
+//   }
+//   if(event.key == 'w'){
+//     w.style.backgroundColor = "white";
+//     w.style.color = "black";
+//   }
+//   if(event.key == 'e'){
+//     e.style.backgroundColor = "white";
+//     e.style.color = "black";
+//   }
+//   if(event.key == 'r'){
+//     r.style.backgroundColor = "white";
+//     r.style.color = "black";
+//   }
+// })
+const keys = document.getElementsByClassName("key");
+
+// 문서 내에서 키다운 이벤트가 발생했을 때
+document.addEventListener("keydown", function(e){
+  let index = 0;
+
+  // e.key : 입력한 키
+  // 문자열.toLowerCase() : 소문자로
+  switch(e.key.toLowerCase()){
+  case 'q': index = 0; break;
+  case 'w': index = 1; break;
+  case 'e': index = 2; break;
+  case 'r': index = 3; break;
+  default: return;
+  }
+
+  keys[index].style.backgroundColor = "skyblue";
 })
-document.addEventListener('keyup', function(event){
-  console.log(event.key);
-  if(event.key == 'q'){
-    q.style.backgroundColor = "white"
-    q.style.color = "black"
-  }
-  if(event.key == 'w'){
-    w.style.backgroundColor = "white";
-    w.style.color = "black";
-  }
-  if(event.key == 'e'){
-    e.style.backgroundColor = "white";
-    e.style.color = "black";
-  }
-  if(event.key == 'r'){
-    r.style.backgroundColor = "white";
-    r.style.color = "black";
-  }
-})
 
-switch(event.key.toLowerCase()){
-  case 'q' : index = 0; break;
-  case 'w' : index = 1; break;
-  case 'e' : index = 2; break;
-  case 'r' : index = 3; break;
-  default : return;
-}
+document.addEventListener("keyup", function(e){
 
-keys[index].
+  let index = 0;
+
+  switch(e.key.toLowerCase()){
+  case 'q': index = 0; break;
+  case 'w': index = 1; break;
+  case 'e': index = 2; break;
+  case 'r': index = 3; break;
+  }
+
+  keys[index].style.backgroundColor = "white";
+});
--- 04_WebFront/3_JavaScript/js/test2.js
+++ 04_WebFront/3_JavaScript/js/test2.js
@@ -1,25 +1,39 @@
-const width = document.getElementById('width');
-const height = document.getElementById('height');
-const fontSize = document.getElementById('font-size');
-const weight = document.querySelector("[name=weight]:checked")
-const weight1 = document.getElementById('weight1');
-const weight2 = document.getElementById('weight2');
-const color = document.getElementById('color');
-const bgColor = document.getElementById('bg-color');
-const left = document.getElementById('left');
-const hCenter = document.getElementById('h-center');
-const right = document.getElementById('right');
-const top1 = document.getElementById('top');
-const vCenter = document.getElementById('v-center');
-const bottom = document.getElementById('bottom');
-const text = document.getElementById('text');
-const output = document.getElementById('output');
-const btn = document.getElementById('btn');
 
 btn.addEventListener('click', function(){
+  const width = document.getElementById('width');
+  const height = document.getElementById('height');
+  const fontSize = document.getElementById('font-size');
+  const weight = document.querySelector("[name=weight]:checked");
+  const color = document.getElementById('color');
+  const bgColor = document.getElementById('bg-color');
+  const hSort = document.querySelector("[name=h-sort]:checked");
+  const vSort = document.querySelector("[name=v-sort]:checked");
+  const text = document.getElementById('text');
+  const output = document.getElementById('output');
+  const btn = document.getElementById('btn');
   output.style.width = width.value + 'px';
   output.style.height = height.value + 'px';
   output.style.fontSize = fontSize.value + 'px';
+  console.log(weight);
+  switch(weight.id){
+    case "weight1": output.style.fontWeight = 'normal'; break;
+    case "weight2": output.style.fontWeight = 'bold'; break;
+  }
   output.style.fontWeight = 
+  output.style.color = color.value;
+  output.style.backgroundColor = bgColor.value;
+  switch(hSort.id){
+    case "left": output.style.justifyContent = "flex-start"; break; 
+    case "h-center": output.style.justifyContent = "center"; break; 
+    case "right": output.style.justifyContent = "flex-end"; break;
+  default: break; 
+  }
+  switch(vSort.id){
+    case "top": output.style.alignItems = "flex-start"; break; 
+    case "v-center": output.style.alignItems = "center"; break; 
+    case "bottom": output.style.alignItems = "flex-end"; break;
+  default: break; 
+  }
+  output.innerText = text.value;
 });
 
