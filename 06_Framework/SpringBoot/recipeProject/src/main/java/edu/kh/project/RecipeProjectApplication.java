4c597780c4ce6eafb23b9725ec1474eb19170d21
FireCurry <firecurry@naver.com>
2023-10-17 14:46:12 +0900

    temp

---
+++ 06_Framework/SpringBoot/recipeProject/src/main/java/edu/kh/project/RecipeProjectApplication.java
@@ -0,0 +1,13 @@
+package edu.kh.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeProjectApplication.class, args);
+	}
+
+}
