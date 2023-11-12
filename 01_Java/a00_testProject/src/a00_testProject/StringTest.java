da86aaf38afe927bdbe79b8c6ef65d6e286878e6
FireCurry <firecurry@naver.com>
2023-11-13 00:18:22 +0900

    ㅁ

---
+++ 01_Java/a00_testProject/src/a00_testProject/StringTest.java
@@ -0,0 +1,13 @@
+package a00_testProject;
+
+public class StringTest {
+
+	public static void main(String[] args) {
+		
+		String string = "123456789";
+		String temp;
+		temp = string.substring(0, 3);
+		
+		System.out.println(temp);
+	}
+}
